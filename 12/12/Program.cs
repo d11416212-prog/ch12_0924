65892024a10c3594f917c85fd745d0144b2523a9
d11416212-prog <d11416212@gs.takming.edu.tw>
2025-09-24 11:15:43 +0800

    v2

--- 12/12/Program.cs
+++ 12/12/Program.cs
@@ -1,13 +1,24 @@
 ﻿using System;
 
-namespace _12
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-            Console.Read();
-        }
+        // 輸入身高(公分)與體重(公斤)
+        Console.Write("請輸入身高(公分): ");
+        double height = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("請輸入體重(公斤): ");
+        double weight = Convert.ToDouble(Console.ReadLine());
+
+        // 換算身高為公尺
+        double heightInMeter = height / 100;
+
+        // 計算 BMI
+        double bmi = weight / (heightInMeter * heightInMeter);
+
+        // 輸出結果
+        Console.WriteLine("您的BMI值為: " + bmi);
+        Console.Read();
     }
 }
