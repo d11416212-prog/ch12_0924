1633eec0a5c160b2bf6effa8a80592237aba71f5
d11416212-prog <d11416212@gs.takming.edu.tw>
2025-09-24 10:42:14 +0800

    V2

---
+++ 12/12/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Read();
+        }
+    }
+}
